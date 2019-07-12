39d86b7de9ee84b752afaed05850b22e265899f6
weirisheng <weirisheng@tax.com>
2019-07-12 16:26:14 +0800

    首次上传

---
+++ AlamofireNetWorkRequest.swift
@@ -0,0 +1,57 @@
+//
+//  AlamofireNetWorkRequest.swift
+//  CocoaPodsManager
+//
+//  Created by Sunrise on 2019/7/12.
+//  Copyright © 2019 ANSO. All rights reserved.
+//
+
+import UIKit
+
+/// 基于Alamofire的二次封装
+class AlamofireNetWorkRequest: NSObject {
+
+    static let shareInstance : AlamofireNetWorkRequest = {
+        
+        let share = AlamofireNetWorkRequest()
+        
+        return share;
+    }()
+    
+    /// 网络请求
+    ///
+    /// - Parameters:
+    ///   - method: 请求方式
+    ///   - urlStr: 请求路径
+    ///   - parameters: 请求参数
+    ///   - requestBlock: 请求返回结果(json数据, 请求返回状态)
+    func NetWorkJsonRequest(method:HTTPMethod,urlStr:String,parameters:NSMutableDictionary, requestBlock : @escaping (_ :JSON, _ :Bool) -> ()){
+        
+        let headers = NSMutableDictionary.init()
+        
+        request(urlStr, method: method, parameters: (parameters as? Parameters), encoding: JSONEncoding.default, headers: (headers as! HTTPHeaders)).responseJSON { (response) in
+            
+            switch response.result{
+                
+            case .success(_):
+                
+                if let jsonString = response.result.value {
+                    
+                    let dataDic = JSON(jsonString)
+                    
+                    requestBlock(dataDic,true)
+                    
+                }else {
+                    
+                    print("JSON转换失败")
+                    requestBlock(JSON.null,false)
+                }
+                
+            case .failure(let error):
+                
+                print("\(method)请求失败:\(error)")
+                requestBlock(JSON.null,false)
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,42 @@
+## 关于
+
+-基于Alamofire网络请求类的二次封装，用于网络请求
+
+## 需求
+
+- iOS 8.0+
+- Xcode 9.0+
+- Swift 4.0+
+
+## 安装
+
+### CocoaPods
+
+```ruby
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.0'
+use_frameworks!
+
+target '<Your Target Name>' do
+pod 'AlamofireNetWorkRequestClass'
+end
+```
+## 用法
+
+```swift
+import AlamofireNetWorkRequestClass
+
+class MyViewController: UIViewController {
+
+    override func viewDidLoad() {
+    
+        super.viewDidLoad()
+        AlamofireNetWorkRequest.shareInstance.NetWorkJsonRequest(method: HTTPMethod.post, urlStr: "www.baidu.com", parameters: ["key":"value"]) { (dataJson, requestState) in
+        
+        
+        }
+
+    }
+```
+
+
